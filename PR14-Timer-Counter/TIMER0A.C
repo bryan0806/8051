32227cdd6f274c53f96c48b4b0101aa9b0daf5fe
Bryan@iii <chunwei.liu@gmail.com>
2014-05-29 18:50:08 +0800

    課堂範例: 使用timer0 mode1 計時器 每0,5s 加一

---
+++ PR14-Timer-Counter/TIMER0A.C
@@ -0,0 +1,26 @@
+//  檔名:TIMER0A.C
+//  配合"TIMER/COUNTER實習板"
+//  利用計時中斷產生計數延遲效果
+//
+#pragma oe db pw(80) SM SB CD
+#include <reg51.h>	//8051初始設定
+char Count ;	//定義字元資料為Count
+
+main()
+{
+ P0=Count=0 ;	//LED全暗
+ TMOD=0x01 ;	//設定Timer0為16位元計時器
+ TH0=(0xffff-50000)/256;	//TH0=0xD8
+ TL0=(0xffff-50000)%256;	//TL0=0xEF
+ EA=1 ;		//致能中斷功能
+ ET0=1 ;	//計時計數器0致能
+ TR0=1 ;	//計時器0開始計時
+ for(;;) ;	//等待計時中斷
+}
+ void timer0(void) interrupt 1
+{
+ Count++ ;	//Count=Count+1,累增一
+ P0=Count ;	//輸出至LED顯示
+ TH0=(0xffff-50000)/256; //TH0=0xD8,還原計時數值
+ TL0=(0xffff-50000)%256; //TL0=0xEF
+}
